e48dfccea88b5c87e3dbf36e63dcaf391a61f92a
sangkny <sangkny@yahoo.com>
2020-07-02 19:10:03 +0900

    update caffe learning rate visualization

---
+++ LR_Visualization.py
@@ -0,0 +1,72 @@
+'''
+training learning rate visualization tools for caffe and etc.
+by sangkny
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_triangular_lr(iteration, stepsize, base_lr, max_lr):
+    """Given the inputs, calculates the lr that should be applicable for this iteration"""
+    cycle = np.floor(1 + iteration/(2  * stepsize))
+    x = np.abs(iteration/stepsize - 2 * cycle + 1)
+    lr = base_lr + (max_lr - base_lr) * np.maximum(0, (1-x))
+    return lr
+
+def get_caffe_lr(mode, iteration, stepsize, base_lr, max_lr, gamma, power, max_iter =100000):
+    """Given the inputs, calculates the lr that should be applicable for this iteration"""
+
+    if(mode =='step'):
+        lr = base_lr*pow(gamma, (np.floor(iteration/stepsize)))
+    elif (mode == 'inv'):
+        lr = base_lr * pow((1 + gamma * iteration), (-power))
+    elif(mode == 'sigmoid'):
+        lr = base_lr * ( 1/(1 + np.exp(-gamma * (iteration - stepsize))))
+    elif(mode == 'exp'):
+        base_lr * pow(gamma, iteration)
+    elif(mode == 'fixed'):
+        lr = base_lr
+    elif(mode=='poly'):
+        lr = base_lr * pow((1 - iteration/max_iter),  (power))
+    elif(mode == 'multistep'):
+        lr = 0
+        print('it is not implemented yet')
+    else:
+        print('please check the mode.')
+
+    return lr
+
+# Demo of how the LR varies with iterations
+num_iterations = 30000
+max_iterations = num_iterations
+#------------ inverse
+base_lr = 0.001
+max_lr = 0.001
+gamma = 0.001
+monentum = 0.9
+power = 0.75
+#---------------------
+# step--------------
+# base_lr = 0.01
+# max_lr = 0.001
+# gamma = 0.1
+# monentum = 0.9
+# power = 0.75
+# -------------------
+stepsize = 10000
+
+lr_trend = list()
+lr_caffe = list()
+mode = 'inv'
+
+print('CAFFE LR mode:' + mode)
+for iter in range(num_iterations):
+    lr = get_triangular_lr(iter, stepsize, base_lr, max_lr)
+    # Update your optimizer to use this learning rate in this iteration
+    lr_trend.append(lr)
+    lr1 = get_caffe_lr(mode=mode, iteration=iter,stepsize=stepsize, base_lr=base_lr, max_lr=max_lr, gamma=gamma, power=power, max_iter=max_iterations)
+    lr_caffe.append(lr1)
+
+#plt.plot(lr_trend)
+plt.plot(lr_caffe)
+plt.show()
